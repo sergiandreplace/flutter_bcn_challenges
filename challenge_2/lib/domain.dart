b8fceec4e5726381402b7d6e3fd81a8b0d39549c
Sergi Martínez <sergi.martinez@scmspain.com>
2018-06-13 20:35:56 +0200

    Basically I forgot to commit for a couple of hours

---
+++ challenge_2/lib/domain.dart
@@ -0,0 +1,60 @@
+
+import 'package:flutter/material.dart';
+
+class Place {
+  final List<Category> categories;
+  final String name;
+  Place(this.name, this.categories);
+
+}
+
+class Category {
+  final String name;
+  final List<Scenery> scenery;
+
+  Category(this.name, this.scenery);
+
+
+}
+
+class Scenery {
+
+  final int id;
+
+  final String title;
+
+  final String city;
+
+  final double area;
+
+  final double population;
+
+  final String description;
+
+  final String image;
+
+  final Color color;
+
+  Scenery(
+      this.id,
+      this.title,
+      this.city,
+      this.area,
+      this.population,
+      this.description,
+      this.image,
+      this.color
+      );
+}
+
+final scenery = [
+  Scenery(1, "Build Scenery", "Venice", 414.75, 34.3, "The necks of the Loch Ness monster-like marine...", "build.jpg", Colors.orange),
+  Scenery(2, "Natural Scenery", "Venice", 414.75, 34.3, "The necks of the Loch Ness monster-like marine...", "natural.jpg", Colors.lightBlue),
+  Scenery(3, "Famous Scenery", "Venice", 414.75, 34.3, "The necks of the Loch Ness monster-like marine...", "famous.jpg", Colors.pink),
+];
+
+final Place venice = Place("Venice",[
+  Category("Scenic", scenery),
+  Category("Restaurant", scenery),
+  Category("Grogshop", scenery),
+]);
---
+++ challenge_2/lib/main.dart
@@ -0,0 +1,213 @@
+import 'package:challenge_2/domain.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: venice.categories.length,
+      child: new Scaffold(
+        appBar: AppBar(
+          title: Text(venice.name),
+          bottom: TabBar(
+            tabs: venice.categories
+                .map((category) => Tab(text: category.name))
+                .toList(),
+          ),
+        ),
+        body: new TabBarView(
+          children: venice.categories
+              .map((category) => TabPage(category: category))
+              .toList(),
+        ),
+      ),
+    );
+  }
+}
+
+class TabPage extends StatelessWidget {
+  final Category category;
+
+  TabPage({Key key, this.category}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) => SceneryRow(category.scenery[index],
+          index.isOdd ? TitleAlignment.left : TitleAlignment.right),
+      itemCount: category.scenery.length,
+    );
+  }
+}
+
+enum TitleAlignment { left, right }
+
+class SceneryRow extends StatelessWidget {
+  final Scenery scenery;
+
+  final TitleAlignment titleAlignment;
+
+  SceneryRow(this.scenery, this.titleAlignment);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
+      height: 200.0,
+      child: InkWell(
+        onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => new SceneryDetailPage(scenery),
+              ),
+            ),
+        child: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 10.0, vertical: 8.0),
+              child: Card(
+                elevation: 4.0,
+                child: new SceneryImage(scenery: scenery),
+              ),
+            ),
+            FractionallySizedBox(
+              alignment: titleAlignment == TitleAlignment.left
+                  ? Alignment.centerLeft
+                  : Alignment.centerRight,
+              widthFactor: 0.75,
+              child: new TitleBox(
+                  titleAlignment: titleAlignment, scenery: scenery),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SceneryDetailPage extends StatelessWidget {
+  final Scenery scenery;
+
+  SceneryDetailPage(this.scenery);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          SceneryDetailBody(scenery),
+          Padding(
+            padding: EdgeInsets.only(
+              top: MediaQuery.of(context).padding.top,
+            ),
+            child: IconButton(
+                icon: Icon(Icons.arrow_back),
+                color: Colors.white,
+                onPressed: () => Navigator.pop(context)),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class SceneryDetailBody extends StatelessWidget {
+  final Scenery scenery;
+
+  SceneryDetailBody(this.scenery);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: EdgeInsets.all(0.0), //check: https://github.com/flutter/flutter/issues/14842
+      children: <Widget>[
+        Container(
+          height: 240.0,
+          child: SceneryImage(scenery: scenery),
+        ),
+        Container(
+          height: 500.0,
+        ),
+      ],
+    );
+  }
+}
+
+class SceneryImage extends StatelessWidget {
+  const SceneryImage({
+    Key key,
+    @required this.scenery,
+  }) : super(key: key);
+
+  final Scenery scenery;
+
+  @override
+  Widget build(BuildContext context) {
+    return Hero(
+      tag: "image-${scenery.id}",
+      child: Image.asset(
+        this.scenery.image,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
+
+class TitleBox extends StatelessWidget {
+  const TitleBox({
+    Key key,
+    @required this.titleAlignment,
+    @required this.scenery,
+  }) : super(key: key);
+
+  final TitleAlignment titleAlignment;
+  final Scenery scenery;
+
+  @override
+  Widget build(BuildContext context) {
+    return Hero(
+      tag: "titleBox-${scenery.id}",
+      child: Container(
+        padding: EdgeInsets.only(
+            left: titleAlignment == TitleAlignment.left ? 64.0 : 32.0),
+        color: scenery.color.withOpacity(0.8),
+        margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              scenery.title.replaceAll(" ", "\n"),
+              style: Theme
+                  .of(context)
+                  .textTheme
+                  .title
+                  .apply(color: Colors.white, fontSizeFactor: 1.2),
+            ),
+            Text(
+              scenery.city,
+              style: Theme
+                  .of(context)
+                  .textTheme
+                  .caption
+                  .apply(color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
